c70273cff68f757cd6b9ef36c5711779980125b7
Tipton Cole <tipton@tipton.com>
2015-08-27 17:26:06 -0500

    Initial commit

---
+++ config.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+'''
+
+Application settings for:
+
+1. Directories
+
+2. Flags
+
+3. Control values
+
+'''
+
+
+
+#-------------------------- Directory Settings ---------------------#
+
+ERLENMEYER_BASE_DIRECTORY = 'path_to/erlenmeyer'
+
+
+
+#-------------------------------- Flags ----------------------------#
+
+
+#--------------------------- Control Values ------------------------#
+
+
+#------------------------- [[ Other Settings]] ---------------------#
---
+++ neck/__init__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+'''
+
+neck is a collection of exemplar materials that demonstrate various methods or
+approaches for implementing standard capabilities of a Flask web application.
+
+Standard Capabilities include:
+
+1. Ability to log to multiple disk files (and stdout) in any module
+
+2. Application landing page
+
+3. User login page
+
+4. Application "action" page
+
+5. Logout function
+
+6. Alignment of client and server cookie management
+
+7. config.py setup
+
+8. Password setting and re-setting
+
+9. Non-session transactions from user
+
+10. Server-sent events
+
+
+'''
+
+from flask import Flask
+
+neck_app = Flask(__name__)
+
